971489738214c067c3bded9cade471672074edff
emoy <emoy.kim@gmail.com>
2019-05-20 22:14:43 +0900

    [110] solved

---
+++ [110]BalancedBinaryTree.cpp
@@ -0,0 +1,76 @@
+﻿/*
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (39.45%)
+ * Total Accepted:    264.2K
+ * Total Submissions: 669.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as:
+ * 
+ * 
+ * a binary tree in which the depth of the two subtrees of every node never
+ * differ by more than 1.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * Return true.
+ * 
+ * Example 2:
+ * 
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * 
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * Return false.
+ * 
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode 
+{
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int maxDepth(TreeNode *root) {
+   if (root == NULL) return 0;
+   return max( 1 + maxDepth( root->left ), 1 + maxDepth( root->right ) );
+}
+
+bool isBalanced(TreeNode* root) {
+   if (root == NULL) return true;
+
+   if (abs( maxDepth( root->left ) - maxDepth( root->right ) ) > 1) return false;
+
+   return isBalanced( root->left ) && isBalanced( root->right );
+}
